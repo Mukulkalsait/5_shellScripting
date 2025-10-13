0562e5092c2e368ea84f208195cffccd8809e23e
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-10-13 21:31:53 +0530

    Updated new shell script.

--- 1_basics/e_loops.sh
+++ 1_basics/e_loops.sh
@@ -6,12 +6,35 @@ EOF
 
 read -p "chose any number in a range of  1-100" num
 
-if ((0 < num && num <= 100)); then
-  x=1
-  while [[ $x -le $num ]]; do
-    echo "$x"
-    ((x++))
+function print_ever_till_given_arg() {
+  for ((y = 0; y <= num; y++)); do
+    if ((y % 2 == 0)); then
+      echo "$y"
+    fi
   done
-else
-  echo "Number is out of range."
-fi
+  echo "--x--"
+}
+
+function print_all_number_till_given_arg() {
+  if ((0 < num && num <= 100)); then
+    x=1
+    while [[ $x -le $num ]]; do
+      echo "$x"
+      ((x++))
+    done
+    echo "--x--"
+  else
+    echo "Number is out of range."
+  fi
+}
+
+function print_information() {
+  echo "--------------------------------------------------------------"
+  echo "|  Calling can only happen after defining function in bash.  |"
+  echo "--------------------------------------------------------------"
+}
+
+# B: function calling:
+print_ever_till_given_arg
+print_all_number_till_given_arg
+print_information
